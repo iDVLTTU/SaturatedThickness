897c671104deac84497f68f23b06a9142e026f88
Hoang Long <litpuvn@gmail.com>
2017-02-07 23:46:05 -0600

    better initialization approach

--- javascripts/js/color.manager.js
+++ javascripts/js/color.manager.js
@@ -3,48 +3,13 @@ idv.colorManager = idv.colorManager || {};
 idv.colorManager.colors = d3.scale.category10();
 
 idv.colorManager.SUPPORTED_COLOR_COUNT = 10;
-idv.colorManager.supportedColors = {
-    "color1": {
+idv.colorManager.supportedColors = {};
+for (var i=0; i< idv.colorManager.SUPPORTED_COLOR_COUNT; i++) {
+    idv.colorManager.supportedColors["color" + (i + 1)] = {
         used: false,
-        code: idv.colorManager.colors(0)
-    },
-    "color2": {
-        used: false,
-        code: idv.colorManager.colors(1)
-    },
-    "color3": {
-        used: false,
-        code: idv.colorManager.colors(2)
-    },
-    "color4": {
-        used: false,
-        code: idv.colorManager.colors(3)
-    },
-    "color5": {
-        used: false,
-        code: idv.colorManager.colors(4)
-    },
-    "color6": {
-        used: false,
-        code: idv.colorManager.colors(5)
-    },
-    "color7": {
-        used: false,
-        code: idv.colorManager.colors(6)
-    },
-    "color8": {
-        used: false,
-        code: idv.colorManager.colors(7)
-    },
-    "color9": {
-        used: false,
-        code: idv.colorManager.colors(8)
-    },
-    "color10": {
-        used: false,
-        code: idv.colorManager.colors(9)
+        code: idv.colorManager.colors(i)
     }
-};
+}
 
 idv.colorManager.getUnusedColorKey = function() {
     var tmpColor;
