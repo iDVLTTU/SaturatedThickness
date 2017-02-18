b61f75d88d1f78070040a624beef646bb0106610
Hoang Long <litpuvn@gmail.com>
2017-02-18 12:07:11 -0600

    add county info to well tooltip

--- javascripts/js/time.chart.manager.js
+++ javascripts/js/time.chart.manager.js
@@ -293,6 +293,39 @@ idv.timeChartManager.generateTimeChart = function(bindToId, columns, colors, typ
                     var format = d3.format(',.2f')
                     return format(value);
                 }
+            },
+            contents: function (d, defaultTitleFormat, defaultValueFormat, color) {
+
+                var $$ = this, config = $$.config,
+                    titleFormat = config.tooltip_format_title || defaultTitleFormat,
+                    nameFormat = config.tooltip_format_name || function (name) { return name; },
+                    valueFormat = config.tooltip_format_value || defaultValueFormat,
+                    text, i, title, value, name, bgcolor;
+                var well;
+                for (i = 0; i < d.length; i++) {
+                    if (! (d[i] && (d[i].value || d[i].value === 0))) { continue; }
+
+                    if (! text) {
+                        title = titleFormat ? titleFormat(d[i].x) : d[i].x;
+                        text = "<table class='" + $$.CLASS.tooltip + "'>" + (title || title === 0 ? "<tr><th colspan='2'>" + title + "</th></tr>" : "");
+                        text += "<tr class='c3tooltipCustomHeader'>";
+                        text += "<th>Well</th>";
+                        text += "<th>S. Thickness</th>";
+                        text += "<th>County</th>";
+                        text += "</tr>";
+                    }
+
+                    name = nameFormat(d[i].name);
+                    value = valueFormat(d[i].value, d[i].ratio, d[i].id, d[i].index);
+                    bgcolor = $$.levelColor ? $$.levelColor(d[i].value) : color(d[i].id);
+                    well = idv.wellMap[ idv.util.getWellIdFromItsName(d[i].id)];
+                    text += "<tr class='" + $$.CLASS.tooltipName + "-" + d[i].id + "'>";
+                    text += "<td class='name'><span style='background-color:" + bgcolor + "'></span>" + name + "</td>";
+                    text += "<td class='value'>" + value + "</td>";
+                    text += "<td class='value'>" + well.detail.county + "</td>";
+                    text += "</tr>";
+                }
+                return text + "</table>";
             }
         }
     });
