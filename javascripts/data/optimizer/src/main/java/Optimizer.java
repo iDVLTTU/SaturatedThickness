8e5e20e71b24701b572e5cf3fde1f886da80cd52
Long Nguyen <litpuvn@gmail.com>
2017-03-01 17:43:05 -0600

    fix pointId in raster to point after conversion

--- javascripts/data/optimizer/src/main/java/Optimizer.java
+++ javascripts/data/optimizer/src/main/java/Optimizer.java
@@ -8,13 +8,15 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Optimizer {
 
 
-    private final static int REDUCE_ROW_FACTOR = 3;
-    private final static int REDUCE_COL_FACTOR = 3;
+    private final static int REDUCE_ROW_FACTOR = 2;
+    private final static int REDUCE_COL_FACTOR = 2;
 
     public static void main(String [] args) {
         System.out.println("Working Directory = " +
@@ -24,7 +26,7 @@ public class Optimizer {
         Iterable<CSVRecord> records = null;
 
         boolean skipHeader = true;
-
+        Map<Integer, Boolean> removedPoints = new HashMap<Integer, Boolean>();
         try {
 
             in = new FileReader("ascii_2013all.csv");
@@ -35,18 +37,20 @@ public class Optimizer {
             CSVPrinter csvFilePrinter = new CSVPrinter(fileWriter, CSVFormat.EXCEL);
 
 
+            int pointId = 0;
             boolean skipRow = false;
             int currentDataRow = 0;
             double cellVal;
             String headerItem;
             List<Double> myNewCsvRecord;
             List<String> headerRecord;
+
+            List<PointPixel> finePoints;
+
             for (CSVRecord record : records) {
 
                 if (skipHeader) {
                     System.out.println("Printing header");
-
-                    skipHeader = false;
 
                     headerRecord = new ArrayList<String>();
                     for(int col=0; col < record.size(); col++) {
@@ -59,12 +63,22 @@ public class Optimizer {
                     }
 
                     csvFilePrinter.printRecord(headerRecord);
-
+                    skipHeader = false;
                 }
                 else {
                     currentDataRow ++;
                     skipRow = (REDUCE_ROW_FACTOR != 0 && (currentDataRow % REDUCE_ROW_FACTOR == 0)) ? true : false;
                     if (skipRow) {
+
+                        for(int col=0; col < record.size(); col++) {
+                            cellVal = Double.parseDouble(record.get(col));
+                            if (cellVal > -9999) {
+                                pointId ++;
+                                removedPoints.put(pointId, true);
+                            }
+
+                        }
+
                         continue;
                     }
 
@@ -72,8 +86,17 @@ public class Optimizer {
                     for(int col=0; col < record.size(); col++) {
                         cellVal = Double.parseDouble(record.get(col));
                         if (REDUCE_COL_FACTOR ==0 || (col % REDUCE_COL_FACTOR) != 0) {
-                            // rowHeader['Var ' +  (col + 1)] = cellVal;
                             myNewCsvRecord.add(cellVal);
+
+                            if (cellVal > -9999) {
+                                pointId ++;
+                            }
+                        }
+                        else {
+                            if (cellVal > -9999) {
+                                pointId ++;
+                                removedPoints.put(pointId, true);
+                            }
                         }
 
                     }
@@ -86,10 +109,92 @@ public class Optimizer {
             fileWriter.close();
             csvFilePrinter.close();
 
+            optimizeRasterToPoint(removedPoints);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+    }
 
+
+
+
+
+    private static void optimizeRasterToPoint(Map<Integer, Boolean> removedPoints) {
+        Reader in = null;
+        Iterable<CSVRecord> records = null;
+
+        boolean skipHeader = true;
+
+        try {
+
+            in = new FileReader("raster_to_point.csv");
+            records = CSVFormat.EXCEL.parse(in);
+
+            String outputFile = System.getProperty("user.dir") + "/raster_to_point.optimized.csv";
+            FileWriter fileWriter = new FileWriter(outputFile);
+            CSVPrinter csvFilePrinter = new CSVPrinter(fileWriter, CSVFormat.EXCEL);
+
+            int tmpPointId;
+            List<String> myNewRecord;
+            int newPointId = 0;
+            for(CSVRecord record : records) {
+
+                if (skipHeader) {
+
+                    csvFilePrinter.printRecord(record); // print header
+                    skipHeader = false;
+                    continue;
+                }
+
+                tmpPointId = Integer.parseInt(record.get(0));
+                if (removedPoints.containsKey(tmpPointId)) {
+                    continue;
+                }
+
+                newPointId ++;
+                myNewRecord = new ArrayList<String>();
+                myNewRecord.add(Integer.toString(newPointId));
+
+                for(int i = 1; i <record.size(); i++) {
+                    myNewRecord.add(record.get(i));
+                }
+
+                csvFilePrinter.printRecord(myNewRecord);
+            }
+
+            fileWriter.flush();
+            fileWriter.close();
+            csvFilePrinter.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
+
+//class PointPixel {
+//    public int id;
+//    public double lon;
+//    public double lat;
+//    public double grid_code;
+//
+//    public PointPixel(int id, double lat, double lon) {
+//        this.id = id;
+//        this.lat = lat;
+//        this.lon = lon;
+//    }
+//
+//    public PointPixel(int id, double lat, double lon, double grid_code) {
+//
+//        this(id, lat, lon);
+//        this.grid_code = grid_code;
+//
+//    }
+//
+//
+//}
